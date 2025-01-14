ee511901417309ce5664510897d2da66821116a0
gagannn3211 <gagandeep.singh@gripinvest.in>
2025-01-14 16:45:43 +0530

    Security added with dotenv

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,11 +1,14 @@
 import {DataSource , DataSourceOptions} from 'typeorm'
+import {config} from 'dotenv'
+
+config()
 export const dataSourceOptions : DataSourceOptions  = {
     type: 'postgres',
-    host: 'localhost' ,
-    port: 5432 ,
-    username: 'postgres',
-    password: 'Gagan@1313',
-    database: 'EcomAPI',
+    host: process.env.DB_HOST ,
+    port: Number(process.env.DB_PORT) ,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: [],
     migrations: [],
     logging: false,
