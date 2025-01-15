e7e823b2cac6d99bd08ec2da04156dedcf23f219
gagannn3211 <gagandeep.singh@gripinvest.in>
2025-01-15 21:09:22 +0530

    Improved user Entity and hashing password for user safety

--- src/users/dto/user-signup.dto.ts
+++ src/users/dto/user-signup.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
-export class UserSignUp{
+export class UserSignUpDto{
     @IsNotEmpty({message:"Name can not be null!!"})
     @IsString({message: "Please provide valid name"})
     name: string;
