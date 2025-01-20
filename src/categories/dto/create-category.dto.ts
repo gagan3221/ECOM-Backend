399da7a76a220f7d72b8bac81a1b49a3fa5fbf13
gagannn3211 <gagandeep.singh@gripinvest.in>
2025-01-20 16:58:15 +0530

    Categories api implemented

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsNotEmpty({ message: 'Title cannot be empty' })
+  @IsString({ message: 'Title should be string' })
+  title: string;
+
+  @IsNotEmpty({ message: 'Description cannot be empty' })
+  @IsString({ message: 'Description should be string' })
+  description: string;
+}
